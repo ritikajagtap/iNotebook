766f71e84b7c06043327283c24bcf175a0b7c15d
ritikajagtap <ritika.jagtap.2002@gmail.com>
2023-03-06 19:44:33 +0530

    assembled router and also included navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is client side!</h1>
+    <div>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route exact path="/" element={<Home />}/>
+          <Route exact path="/about" element={<About />}/> 
+        </Routes>
+      </Router>
     </div>
   );
 }
